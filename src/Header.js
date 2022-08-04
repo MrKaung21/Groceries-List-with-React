041d4be6e68d9277a589ab533d96da5326152345
Kaung Myat Tun <kaungtun111@gmail.com>
2022-08-04 23:12:46 +0630

    Props and Prop Drilling

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
-export const Header = () => {
+export const Header = ({title}) => {
    
     return (
         <header>
-            <h1>Groceries List</h1>
+            <h1>{title}</h1>
         </header>
     )
+}
+
+Header.defaultProps = {
+    title: "Groceries List"
 }
 
 export default Header
